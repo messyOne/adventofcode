bc072bbd2eaf0126a919fc6e161758954384bdea
messyOne <github@weiss-daniel.de>
2019-12-05 22:34:07 +0100

    Create 4-2.scala

---
+++ 4-2.scala
@@ -0,0 +1,10 @@
+var count = 0
+
+for (i <- 171309 to 643603) {
+  if (i.toString.groupBy(c => c.toLower).map(e => e._2.length).toSet.contains(2)
+    && i.toString.sorted.toInt == i)
+    count = count + 1
+}
+
+count
+
