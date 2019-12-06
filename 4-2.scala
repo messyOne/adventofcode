a6994961885eab55e39af42605b82dd1fe94ae7c
messyOne <github@weiss-daniel.de>
2019-12-06 10:09:23 +0100

    Update 4-2.scala

--- 4-2.scala
+++ 4-2.scala
@@ -1,10 +1,9 @@
 var count = 0
 
 for (i <- 171309 to 643603) {
-  if (i.toString.groupBy(c => c.toLower).map(e => e._2.length).toSet.contains(2)
+  if (i.toString.groupBy(c => c).map(_._2.length).toSet.contains(2)
     && i.toString.sorted.toInt == i)
     count = count + 1
 }
 
 count
-
