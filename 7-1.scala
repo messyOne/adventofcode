587e73d5820f68c3ff05e1ce1274750067d3e6a7
messyOne <github@weiss-daniel.de>
2019-12-11 18:46:46 +0100

    Update 7-1.scala

--- 7-1.scala
+++ 7-1.scala
@@ -1,3 +1,5 @@
+import IntcodeComputer.State
+
 object Main extends App {
   val values = "3,8,1001,8,10,8,105,1,0,0,21,42,67,76,89,110,191,272,353,434,99999,3,9,102,2,9,9,1001,9,2,9,1002,9,2,9,1001,9,2,9,4,9,99,3,9,1001,9,4,9,102,4,9,9,101,3,9,9,1002,9,2,9,1001,9,4,9,4,9,99,3,9,102,5,9,9,4,9,99,3,9,1001,9,3,9,1002,9,3,9,4,9,99,3,9,102,3,9,9,101,2,9,9,1002,9,3,9,101,5,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,99"
   private val valuesList = values
@@ -6,7 +8,8 @@ object Main extends App {
   val winner = List(0, 1, 2, 3, 4).permutations.foldLeft((0, List.empty[Int])) {
     case ((throttle, combination), c) =>
       val newThrottle = c.foldLeft(0) {
-        case (throttle, setting) => IntcodeComputer().exec(valuesList, List(setting, throttle)).head
+        case (throttle, setting) =>
+          new IntcodeComputer().run(State(valuesList, List(setting, throttle))).output.head
       }
 
       if (newThrottle > throttle) {
@@ -16,11 +19,22 @@ object Main extends App {
       }
   }
 
-  print(winner)
+  print(winner._1)
 }
 
-case class IntcodeComputer() {
+object IntcodeComputer {
+  case class State(program: List[Int], input: List[Int], pointer: Int = 0, output: List[Int] = Nil) {
+    def writeOutput(value: Int): State = copy(output = output :+ value)
+    def readInput(): (List[Int], State) = (List(input.head), copy(input = input.tail))
+    def updateProgram(program: List[Int]): State = copy(program = program)
+    def increasePointer(by: Int): State = copy(pointer = pointer + by)
+    def pointer(pointer: Int): State = copy(pointer = pointer)
+    def step: Int = program(pointer)
+    def arg(number: Int) : Int = program(pointer + number)
+  }
+}
 
+class IntcodeComputer() {
   import scala.annotation.tailrec
 
   private def splitOp(op: Int) = {
@@ -38,88 +52,90 @@ case class IntcodeComputer() {
     } else 0
   }
 
-  private def param(l: List[Int], p: Int, modes: Int, arg: Int, forcePositionMode: Boolean = false) = {
-    if (mode(modes, arg) == 0 && !forcePositionMode)
-      l(l(p + arg)) else
-      l(p + arg)
+  private def param(state: State, modes: Int, arg: Int, forcePositionMode: Boolean = false) = {
+    if (mode(modes, arg) == 0 && !forcePositionMode) {
+      state.program(state.arg(arg))
+    } else {
+      state.arg(arg)
+    }
   }
 
-  def exec(values: List[Int], input: List[Int]): List[Int] = iterate(values, input)
+  def run(state: State): State = iterate(state)
 
   @tailrec
-  private def iterate(l: List[Int], input: List[Int], output: List[Int] = Nil, p: Int = 0): List[Int] = {
-    val (operator, modes) = splitOp(l(p))
+  private def iterate(state: State): State = {
+    val (operator, modes) = splitOp(state.step)
 
     operator match {
-      case 99 => output
+      case 99 => state
       case 1 =>
-        val op1 = param(l, p, modes, 1)
-        val op2 = param(l, p, modes, 2)
-        val op3 = param(l, p, modes, 3, forcePositionMode = true)
-
-        val updatedList = l.patch(op3, List(op1 + op2), 1)
-        iterate(updatedList, input, output, p + 4)
+        val op1 = param(state, modes, 1)
+        val op2 = param(state, modes, 2)
+        val op3 = param(state, modes, 3, forcePositionMode = true)
+        
+        val updatedProgram = state.program.patch(op3, List(op1 + op2), 1)
+        iterate(state.updateProgram(updatedProgram).increasePointer(4))
       case 2 =>
-        val op1 = param(l, p, modes, 1)
-        val op2 = param(l, p, modes, 2)
-        val op3 = param(l, p, modes, 3, forcePositionMode = true)
+        val op1 = param(state, modes, 1)
+        val op2 = param(state, modes, 2)
+        val op3 = param(state, modes, 3, forcePositionMode = true)
 
-        val updatedList = l.patch(op3, List(op1 * op2), 1)
-
-        iterate(updatedList, input, output, p + 4)
+        val updatedProgram = state.program.patch(op3, List(op1 * op2), 1)
+        iterate(state.updateProgram(updatedProgram).increasePointer(4))
       case 3 =>
-        val op1 = param(l, p, modes, 1, forcePositionMode = true)
+        val op1 = param(state, modes, 1, forcePositionMode = true)
 
-        val updatedList = l.patch(op1, List(input.head), 1)
-        iterate(updatedList, input.tail, output, p + 2)
+        val (input, newState) = state.readInput()
+        val updatedProgram = newState.program.patch(op1, input, 1)
+        iterate(newState.updateProgram(updatedProgram).increasePointer(2))
       case 4 =>
-        val op1 = param(l, p, modes, 1)
+        val op1 = param(state, modes, 1)
 
-        iterate(l, input, output :+ op1, p + 2)
+        iterate(state.writeOutput(op1).increasePointer(2))
       case 5 =>
-        val op1 = param(l, p, modes, 1)
-        val op2 = param(l, p, modes, 2)
+        val op1 = param(state, modes, 1)
+        val op2 = param(state, modes, 2)
 
         if (op1 != 0) {
-          iterate(l, input, output, op2)
+          iterate(state.pointer(op2))
         } else {
-          iterate(l, input, output, p + 3)
+          iterate(state.increasePointer(3))
         }
       case 6 =>
-        val op1 = param(l, p, modes, 1)
-        val op2 = param(l, p, modes, 2)
+        val op1 = param(state, modes, 1)
+        val op2 = param(state, modes, 2)
 
         if (op1 == 0) {
-          iterate(l, input, output, op2)
+          iterate(state.pointer(op2))
         } else {
-          iterate(l, input, output, p + 3)
+          iterate(state.increasePointer(3))
         }
       case 7 =>
-        val op1 = param(l, p, modes, 1)
-        val op2 = param(l, p, modes, 2)
-        val op3 = param(l, p, modes, 3, forcePositionMode = true)
+        val op1 = param(state, modes, 1)
+        val op2 = param(state, modes, 2)
+        val op3 = param(state, modes, 3, forcePositionMode = true)
 
-        val updatedList = if (op1 < op2) {
-          l.patch(op3, List(1), 1)
+        val updatedProgram = if (op1 < op2) {
+          state.program.patch(op3, List(1), 1)
         } else {
-          l.patch(op3, List(0), 1)
+          state.program.patch(op3, List(0), 1)
         }
 
-        iterate(updatedList, input, output, p + 4)
+        iterate(state.updateProgram(updatedProgram).increasePointer(4))
       case 8 =>
-        val op1 = param(l, p, modes, 1)
-        val op2 = param(l, p, modes, 2)
-        val op3 = param(l, p, modes, 3, forcePositionMode = true)
+        val op1 = param(state, modes, 1)
+        val op2 = param(state, modes, 2)
+        val op3 = param(state, modes, 3, forcePositionMode = true)
 
-        val updatedList = if (op1 == op2) {
-          l.patch(op3, List(1), 1)
+        val updatedProgram = if (op1 == op2) {
+          state.program.patch(op3, List(1), 1)
         } else {
-          l.patch(op3, List(0), 1)
+          state.program.patch(op3, List(0), 1)
         }
 
-        iterate(updatedList, input, output, p + 4)
+        iterate(state.updateProgram(updatedProgram).increasePointer(4))
 
-      case x => throw new RuntimeException(s"Unknown opcode: $x, Pointer: $p")
+      case x => throw new RuntimeException(s"Unknown opcode: $x, Pointer: ${state.pointer}")
     }
   }
 }
