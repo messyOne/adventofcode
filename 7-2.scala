63960979897897926a74ca97ecfc7a9feb7782b4
messyOne <github@weiss-daniel.de>
2019-12-12 15:42:24 +0100

    Update 7-2.scala

--- 7-2.scala
+++ 7-2.scala
@@ -1,5 +1,6 @@
 import IntcodeComputer.State
 
+// TODO not working
 object Main extends App {
   val values = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
   private val valuesList = values
