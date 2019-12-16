d7e7fd527f3d659ffdb31b44684130df5387097c
messyOne <github@weiss-daniel.de>
2019-12-16 23:09:50 +0100

    Update 10-2.scala

--- 10-2.scala
+++ 10-2.scala
@@ -1,7 +1,6 @@
-import com.sun.tools.classfile.TypeAnnotation.Position
 
 object Main extends App {
-  val input: Seq[List[Char]] = ".#....#####...#..\n##...##.#####..##\n##...#...#.#####.\n..#.....#...###..\n..#.#.....#....##".split("\n").toList.map(_.toList)
+  val input: Seq[List[Char]] = ".#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##".split("\n").toList.map(_.toList)
   case class Asteroid(position: (Int, Int), distance: Double, arc: Double, group: Int = -1)
 
   def analyze(position: (Int, Int)) = {
@@ -10,7 +9,7 @@ object Main extends App {
         row.zipWithIndex.filter(cx => cx._1 == '#').map(_._2).foldLeft(asteroids) {
           case (asteroids, x) if position == (y, x) => asteroids
           case (asteroids, x) =>
-            val arc = Math.atan2(position._2-y, position._1-x)
+            val arc = Math.atan2(position._1-y, position._2-x)
             val distance = Math.sqrt((position._1-x)*(position._1-x) + (position._2-y)*(position._2-y))
 
             asteroids :+ Asteroid((y, x), distance, arc)
@@ -18,27 +17,24 @@ object Main extends App {
     }
   }
 
-  //  val result = analyze((25,31)).sortBy(_.distance).groupBy(_.arc).toSeq.sortBy(_._1).map(x => x._1 -> x._2.sortBy(_.distance)).partition(_._1 >= 1.5707963267948966)
-
-  val result = analyze(8, 3).sortBy(a => (a.arc, a.distance)).partition(_.arc >= Math.PI/2)
+  val result = analyze(13, 11).sortBy(a => (a.arc, a.distance)).partition(_.arc >= Math.PI/2)
 
   @scala.annotation.tailrec
   def traverse(asteroids: List[Asteroid], count: Int = 0, arcBefore: Double = Double.MaxValue): Asteroid = {
     val a = asteroids.head
 
-    if (count == 200) {
+    if (count == 199) {
       a
     } else {
       if (a.arc == arcBefore) {
         traverse(asteroids.tail :+ asteroids.head, count, arcBefore)
       } else {
-        print(a.position, "\n")
         traverse(asteroids.tail, count + 1, a.arc)
       }
     }
   }
-//
-//  print(traverse((result._2 ::: result._1).reverse, 1).position) // TODO check with reverse
 
-  (result._2 ::: result._1).foreach(a => print(a, "\n"))
+//  (result._1 ::: result._2).zipWithIndex.foreach(a => print(s"${a._2+1} => ${a._1.position.swap} - ${a._1.arc} - ${a._1.distance} \n"))
+
+  print(traverse(result._1 ::: result._2))
 }
